08ce65fe7aedcedef139473d1dd873d27bcfd60c
tianzx <436011080@qq.com>
2016-05-15 21:49:38 +0800

    class traditonal

--- class/class.js
+++ class/class.js
@@ -10,7 +10,7 @@ function User(name,age) {
 
 //static function
 User.getClassName = function() {
-    return 'User';
+    return 'User,tianzx';
 };
 
 User.prototype.changeName = function(name){
@@ -20,5 +20,35 @@ User.prototype.changeName = function(name){
 User.prototype.changeAge  = function(name){
     this.age = age;
 };
+
+Object.defineProperty(User.prototype,'info',{
+    get(){
+        return 'name:' + this.name +' | age:' +this.age;
+    }
+});
+
+var user = new User('tianzx',22);
+console.log(user);
+console.log(User.getClassName());
+
+//child class
+function Manager(name,age,password) {
+    User.call(this.name,age);
+    this.password = password;
+};
+
+//extends static function
+Manage.__proto__= User;
+
+//extends prototype function
+Manager.prototype = User.prototype;
+
+Manager.prototype.changePassword= function(pwd) {
+    this.password = pwd;
+};
+
+var manager = new Manager('leo',22,'123');
+
+
 
 
