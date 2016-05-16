79d38e5acc5a347f864595dffec6911e720e403a
tianzx <436011080@qq.com>
2016-05-16 20:56:58 +0800

    promise solve async

---
+++ promises/promises.js
@@ -0,0 +1,32 @@
+/**
+ * Created by tianzx on 2016/5/16.
+ */
+
+function asyncFun(a,b,cb){
+    setTimeout(function(){
+        cb(a+b);
+    },200);
+}
+
+asyncFun(1,2,function(result){
+    console.log(result);
+})
+
+console.log(2);
+
+function asyncFun2(a,b){
+    return new Promise(function(resolve,reject){
+        setTimeout(function(){
+            resolve(a+b);
+        },200);
+    });
+}
+asyncFun2(1,2).then(function(result){
+    if(result>2){
+        return asyncFun2(result,2)
+    }
+}).then(function(result){
+    if(result>4){
+        console.log('ok');
+    }
+})
