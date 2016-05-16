b7871388fc7c131f4295bf8a143948db7b54415e
tianzx <436011080@qq.com>
2016-05-16 21:12:23 +0800

    promise solve async

--- promises/promises.js
+++ promises/promises.js
@@ -16,17 +16,25 @@ console.log(2);
 
 function asyncFun2(a,b){
     return new Promise(function(resolve,reject){
+
+        if(typeof a!=='number' || typeof  b!=='number') {
+            reject(new Error("no number"));
+        }
         setTimeout(function(){
             resolve(a+b);
         },200);
     });
 }
-asyncFun2(1,2).then(function(result){
+asyncFun2(1,'a').then(function(result){
     if(result>2){
         return asyncFun2(result,2)
     }
+},function(error){
+    console.log('first -',error)
 }).then(function(result){
     if(result>4){
         console.log('ok');
     }
+}).catch(function(error){
+    console.log(error);
 })
